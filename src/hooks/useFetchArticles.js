db8f472a78d94f203f291cb594d22a13d7d4b3e6
Karim Beiranvand <karim@Karims-MacBook-Pro.local>
2022-11-29 14:13:38 +0100

    fetch articles hook implementation

---
+++ src/hooks/useFetchArticles.js
@@ -0,0 +1,44 @@
+import { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+import { EnvironmentVariables } from '../environmentVariables';
+
+export const useFetchArticles = () => {
+  const [articles, setArticles] = useState(null);
+  const [error, setError] = useState(null);
+  const [articlesLoading, setArticlesLoading] = useState(false);
+  const { DATA_SOURCE_NETWORK_URL } = useContext(EnvironmentVariables);
+
+  const fetchArticles = async () => {
+    try {
+      if (!DATA_SOURCE_NETWORK_URL) {
+        return setError(new Error('Network data source URL is missing.'));
+      }
+      setArticlesLoading(true);
+      const res = await axios.get(`${DATA_SOURCE_NETWORK_URL}/code-assignment/articles`, {
+        headers: {
+          market: 'SE',
+          locale: 'sv-SE'
+        }
+      });
+      setArticles(res.data.articles);
+    } catch (capturedError) {
+      /**
+       * log the error to sentry, kibana ...
+       * for example:
+       * log({type: 'error', message: 'failed to fetch articles', data: {capturedError, userId})
+       */
+      setError(capturedError);
+    } finally {
+      setArticlesLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  const articlesError = error && 'Failed to fetch articles.';
+
+  return { articles, articlesError, articlesLoading };
+};
