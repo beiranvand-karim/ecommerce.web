cddba081a3dcfffd4c3267c5796723a256959560
Karim Beiranvand <beiranvand.karim@gmail.com>
2022-12-07 15:42:42 +0100

    bring articles and filters section together to create home page (#21)

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,22 @@
 import { filteringConfigurations, FilteringConfigurations } from '../injectables';
 
+import { ArticlesSection } from '../components/articles';
+import { FiltersSection } from '../components/filters';
+import { useFetchArticles } from '../hooks';
+
 export const HomePage = () => {
+  const { articles, articlesError, articlesLoading, filterArticlesByPriceAmount } =
+    useFetchArticles();
+
   return (
     <FilteringConfigurations.Provider value={filteringConfigurations}>
-      <span>Home page</span>
+      {articles && <FiltersSection filterArticlesByPriceAmount={filterArticlesByPriceAmount} />}
+      <ArticlesSection
+        articles={articles}
+        articlesError={articlesError}
+        articlesLoading={articlesLoading}
+        filterArticlesByPriceAmount={filterArticlesByPriceAmount}
+      />
     </FilteringConfigurations.Provider>
   );
 };
